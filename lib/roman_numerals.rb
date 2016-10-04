0cf5969d443562321dd8aabee03bc85cc9aa034e
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-10-04 22:52:18 +0100

    Change code blocks from ifs to whiles

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -3,27 +3,27 @@ class ArabicToRoman
 
     romanValue = ""
 
-    if number >= 10
+    while number >= 10
       romanValue << "X"
       number -= 10
     end
 
-    if number >= 9
+    while number >= 9
       romanValue << "IX"
       number -= 9
     end
 
-    if number >= 5
+    while number >= 5
       romanValue << "V"
       number -= 5
     end
 
-    if number >= 4
+    while number >= 4
       romanValue << "IV"
       number -= 4
     end
      
-    if number >= 1
+    while number >= 1
       romanValue << "I"
       number -= 1
     end
