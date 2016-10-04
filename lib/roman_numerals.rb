209352ba18e3e46b2d41af3cc856f4d368d39e14
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-10-04 23:29:44 +0100

    Refactor code to use array and while loop

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -3,32 +3,20 @@ class ArabicToRoman
 
     romanValue = ""
 
-    while number >= 10
-      romanValue << "X"
-      number -= 10
-    end
+    numbers = [
+      [10, "X"],
+      [9, "IX"],
+      [5, "V"],
+      [4, "IV"],
+      [1, "I"]
+    ]
 
-    while number >= 9
-      romanValue << "IX"
-      number -= 9
+    numbers.each do |num, roman|
+      while number >= num
+        romanValue << roman
+        number -= num
+      end
     end
-
-    while number >= 5
-      romanValue << "V"
-      number -= 5
-    end
-
-    while number >= 4
-      romanValue << "IV"
-      number -= 4
-    end
-     
-    while number >= 1
-      romanValue << "I"
-      number -= 1
-    end
-    
-    return romanValue
-    
+    romanValue
   end
 end
