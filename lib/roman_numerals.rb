680b3de834041e6f6bfd4ee9ebc68f46434a5859
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-10-26 23:03:39 +0100

    Completed kata

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -4,6 +4,12 @@ class ArabicToRoman
     romanValue = ""
 
     numbers = [
+      [1000, "M"],
+      [900, "CM"],
+      [500, "D"],
+      [100, "C"],
+      [90, "XC"],
+      [50, "L"],
       [10, "X"],
       [9, "IX"],
       [5, "V"],
@@ -11,10 +17,10 @@ class ArabicToRoman
       [1, "I"]
     ]
 
-    numbers.each do |num, roman|
-      while number >= num
+    numbers.each do |arabic, roman|
+      while number >= arabic
         romanValue << roman
-        number -= num
+        number -= arabic
       end
     end
     romanValue
